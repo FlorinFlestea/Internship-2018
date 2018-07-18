da4a5be8c554487fe6f415b839a9908909666c2b
mihais98 <mihais98@users.noreply.github.com>
2018-07-18 09:57:17 +0300

    ProiectChange

---
+++ BusinessTripApplication/BusinessTripApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BusinessTripApplication.MvcApplication" Language="C#" %>
---
+++ BusinessTripApplication/BusinessTripApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BusinessTripApplication.Startup))]
+namespace BusinessTripApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
