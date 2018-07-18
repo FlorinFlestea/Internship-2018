e6a1cff22d23ed4174092b4b37ae042ace657c27
Sabau Andrei <sabau_andrei1998@yahoo.com>
2018-07-18 14:56:52 +0300

    UnitTests project

---
+++ BusinessTripApplication.UnitTests/Models/UserModelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessTripApplication.UnitTests.Models
+{
+    [TestClass]
+    public class UserModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
