84f7deb9daa041c7def7ecd2481d31cdea113a3c
Alex Cernov <cernovalex1@gmail.com>
2018-07-18 11:10:50 +0300

    Nefinalizat

--- BusinessTripApplication/Controllers/UserController.cs
+++ BusinessTripApplication/Controllers/UserController.cs
@@ -11,8 +11,12 @@ namespace Registration.Controllers
         // GET: User
         public ActionResult Registration(int id=0)
         {
-            User userModel = new User();
-
+            User userModel = new User("Alex Cernov","cernovalex1@gmail.com","test123");
+            using (UserContext db = new UserContext())
+            {
+                db.Users.Add(userModel);
+                db.SaveChanges();
+            }
             return View(userModel);
         }
     }
