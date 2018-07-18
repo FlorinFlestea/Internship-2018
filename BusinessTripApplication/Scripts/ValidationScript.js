e7dc2ce8edcbdcda408efcc94c9acbea7a0e7c8f
csaba97 <37183688+csaba97@users.noreply.github.com>
2018-07-18 20:07:19 +0300

    Bug Fixes in Client Side validation

--- BusinessTripApplication/Scripts/ValidationScript.js
+++ BusinessTripApplication/Scripts/ValidationScript.js
@@ -15,16 +15,16 @@
     });
 
     function CheckUsername() {
-        var username = $("#Name").val;
-        var usernamePattern = new RegExp(/^[a-zA-Z0-9]/);
+        var username = $("#Name").val();
+        var usernamePattern = new RegExp("^[a-zA-Z0-9]+$");
         var outputString = "";
         $("#username_error_message").hide();
 
-        if (username.length < 5 && username.length > 20) {
-            var outputString = "The length should be between 5 - 20 characters\ ";
+        if (username.length < 5 || username.length > 20) {
+            outputString = "The length should be between 5 - 20 characters";
         }
-        if (username.match(usernamePattern) === true) {
-            outputString = " Allowed characters: [a-Z],[0-9]\ ";
+        else if (usernamePattern.test(username) === false) {
+            outputString = "Allowed characters: a-z, A-Z, 0-9";
         } 
 
         if (outputString.length > 0) {
@@ -36,26 +36,23 @@
             error_username = false;
         }
 
-           
     }
     function CheckPassword() {
-        var password = $("#Password").val;       
+        var password = $("#Password").val();       
         var outputString = "";
         var passwordPattern1 = new RegExp(/[a-z]/);
         var passwordPattern2 = new RegExp(/[A-Z]/);
         var passwordPattern3 = new RegExp(/[0-9]/);
         $("#password_error_message").hide();
-        if (password.length >= 8 && password.length <= 24) {
-            outputString += "should be between 8 - 24 characters\ ";
-        } else {
-        }
-        if (passwordPattern1.test(password) === false) {
-            outputString += "The string must contain at least 1 lowercase alphabetical character\ ";
-        } else if (passwordPattern2.test(password)) {
-            outputString += "The string must contain at least 1 uppercase alphabetical character\ ";
-        
-        } else if (passwordPattern3.test(password)) {
-            outputString += "The string must contain at least 1 numeric character\ ";
+        if (password.length < 8 || password.length > 24) {
+            outputString = "should be between 8 - 24 characters<br>";
+        } 
+        else if (passwordPattern1.test(password) === false) {
+            outputString = "The string must contain at least 1 lowercase alphabetical character";
+        } else if (passwordPattern2.test(password) === false) {
+            outputString = "The string must contain at least 1 uppercase alphabetical character";
+        } else if (passwordPattern3.test(password) === false) {
+            outputString = "The string must contain at least 1 numeric character";
         }
         if (outputString.length > 0) {
             error_password = true;
@@ -67,7 +64,7 @@
         }
     }
     function CheckEmail() {
-        var email = $("#Email").val;
+        var email = $("#Email").val();
         var emailPattern = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
         $("#email_error_message").hide();
         if (emailPattern.test(email) === false) {
