0260a36e4f5511f1f1d1ad6ae1f8bb277a65538b
Alex Cernov <cernovalex1@gmail.com>
2018-07-19 16:10:00 +0300

    Revert "Revert "Registration method in controller changed -> Added RegisterViewModel and modify the View so it uses the ViewModel.""
    This reverts commit f999db633ad164bf28b5ced0b0ee88705a0f9261.

--- BusinessTripApplication/Controllers/UserController.cs
+++ BusinessTripApplication/Controllers/UserController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Mail;
 using System.Web.Mvc;
 using BusinessTripApplication.Models;
 using BusinessTripApplication.Repository;
+using BusinessTripApplication.ViewModels;
 
 namespace BusinessTripApplication.Controllers
 {
@@ -27,74 +25,24 @@ namespace BusinessTripApplication.Controllers
         [HttpGet]
         public ActionResult Registration()
         {
-            return View();
+            RegistrationViewModel model = new RegistrationViewModel();
+            return View(model);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Registration([Bind(Exclude = "IsEmailVerified,ActivationCode")] User user)
         {
-            if (ModelState.IsValid)
+            try
             {
-                var isExist = UserRepository.EmailExists(user.Email);
-                if (isExist)
-                {
-                    ViewBag.Message = "Email already in the database !";
-                    ViewBag.Status = false;
-                    return View(user);
-                }
-
-                user = UserRepository.Add(user);
-
-                //Send Email to User
-                SendVerificationLinkEmail(user.Email, user.ActivationCode.ToString());
-                ViewBag.Message = "Registration successfully done. Account activation link " +
-                            " has been sent to your email id:" + user.Email;
-                ViewBag.Status = true;
-                return View(user);
-
+                RegistrationViewModel model = new RegistrationViewModel(ModelState.IsValid, user,UserRepository);
+                return View(model);
             }
-            else
+            catch (Exception e)
             {
-                ViewBag.Message = "Invalid request";
-                ViewBag.Status = false;
-                return View(user);
+                return RedirectToRoute("~/Shared/Error");
             }
-        }
-
-
-        [NonAction]
-        public void SendVerificationLinkEmail(string emailID, string activationCode)
-        {
-            var verifyUrl = "/User/VerifyAccount/" + activationCode;
-            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
-
-            var fromEmail = new MailAddress("businesstripapplication@gmail.com", "Registration");
-            var toEmail = new MailAddress(emailID);
-            var fromEmailPassword = "ParolaTest1234"; // Replace with actual password
-            string subject = "Your account is successfully created!";
-
-            string body = "<br/><br/>We are excited to tell you that your account is" +
-                          " successfully created. Please click on the below link to verify your account" +
-                          " <br/><br/><a href='" + link + "'>" + link + "</a> ";
-
-            var smtp = new SmtpClient
-            {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
-            };
-
-            using (var message = new MailMessage(fromEmail, toEmail)
-            {
-                Subject = subject,
-                Body = body,
-                IsBodyHtml = true
-            })
-                smtp.Send(message);
+            
         }
 
         [HttpGet]
