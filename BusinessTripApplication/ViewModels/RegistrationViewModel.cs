e3fe6fc8c9923e0c3928f898485ade450a2b8cd8
Sabau Andrei <sabau_andrei1998@yahoo.com>
2018-07-20 11:24:22 +0300

    InternetCoonectionCheck

--- BusinessTripApplication/ViewModels/RegistrationViewModel.cs
+++ BusinessTripApplication/ViewModels/RegistrationViewModel.cs
@@ -6,6 +6,7 @@ using System.Net.Mail;
 using System.Web;
 using BusinessTripApplication.Models;
 using BusinessTripApplication.Repository;
+using BusinessTripApplication.Tools;
 
 namespace BusinessTripApplication.ViewModels
 {
@@ -54,9 +55,10 @@ namespace BusinessTripApplication.ViewModels
         }
         public void SendVerificationLinkEmail(string emailId, string activationCode)
         {
+            if (!InternetConnection.CheckForInternetConnection())
+                return;
 
             string domainName = "http://localhost:54301";
-
 
             var link = domainName + "/User/VerifyAccount/" + activationCode;
 
