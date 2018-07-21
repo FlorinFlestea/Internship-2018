761cb108e183a29958456bb61f1a555aaaf512f7
cosmin <roman.cosmin97@yahoo.com>
2018-07-21 16:03:23 +0200

    f

---
+++ BusinessTripApplication/Models/UserLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BusinessTripApplication.Models
+{
+    public class UserLogin
+    {   
+        [Display(Name="Email ID")]
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Email required")]
+        public string EmailId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
