8c515a6f9abe32cbc7a4dd68d461c0ad47a1a4a7
Cojanu Dragos <dragoscojanu97@yahoo.ro>
2018-07-25 16:21:17 +0300

    Add trip service

---
+++ BusinessTripApplication/Service/ITripService.cs
@@ -0,0 +1,13 @@
+﻿
+using BusinessTripApplication.Models;
+
+namespace BusinessTripApplication.Service
+{
+    public interface ITripService
+    {
+        Trip Add(Trip addedTrip);
+        void Aprove(Trip trip);
+        void Deny(Trip trip);
+        void Remove(Trip trip);
+    }
+}
