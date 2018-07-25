a8e62431939d2b99f117a622f78d90f4ddb28ba5
Cojanu Dragos <dragoscojanu97@yahoo.ro>
2018-07-25 16:01:20 +0300

    Add trip repository

---
+++ BusinessTripApplication/Repository/ITripRepository.cs
@@ -0,0 +1,14 @@
+﻿using BusinessTripApplication.Models;
+using System.Collections.Generic;
+
+namespace BusinessTripApplication.Repository
+{
+    public interface ITripRepository
+    {
+        Trip Add(Trip addedTrip);
+        IList<Trip> FindAll();
+        Trip Update(Trip updatedTrip);
+        void Remove(Trip deleteTrip);
+
+    }
+}
