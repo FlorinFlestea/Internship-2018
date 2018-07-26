dcc3884d9f27ebf00f047bcdc836594779b9f4f3
mihais98 <mihais98@users.noreply.github.com>
2018-07-26 09:38:22 +0300

    redesign index intro,dashbusinesstrip

---
+++ BusinessTripApplication/Models/UserLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BusinessTripApplication.Models
+{
+    public class UserLogin
+    {
+        [Display(Name = "Email Id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email Id Required")]
+        public string EmailId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email Id Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+
+    }
+}
