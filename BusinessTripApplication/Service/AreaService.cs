dc9f805887ed9a0cf102c212f755a572522bd561
Cojanu Dragos <dragoscojanu97@yahoo.ro>
2018-07-26 10:26:06 +0300

    Area Service

---
+++ BusinessTripApplication/Service/AreaService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BusinessTripApplication.Models;
+using BusinessTripApplication.Repository;
+
+namespace BusinessTripApplication.Service
+{
+    public class AreaService : IAreaService
+    {
+        readonly IAreaRepository areaRepository;
+
+        public AreaService()
+        {
+            this.areaRepository = new AreaRepository();
+        }
+
+        public AreaService(IAreaRepository areaRepository)
+        {
+            this.areaRepository = areaRepository;
+        }
+
+        public IList<Area> FindAll()
+        {
+            try
+            {
+                return areaRepository.FindAll(); 
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ BusinessTripApplication/Service/IAreaService.cs
@@ -0,0 +1,13 @@
+﻿using BusinessTripApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessTripApplication.Service
+{
+    public interface IAreaService
+    {
+        IList<Area> FindAll();
+    }
+}
