da59542edf926c258ade4f9930186a7071fadbe6
Cernov Alex <cernovalex1@gmail.com>
2018-07-31 14:27:52 +0300

    Added ApiClient interface

---
+++ BusinessTripAdministration/Models/ApiClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessTripApplication.Models;
+
+namespace BusinessTripAdministration.Models
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly string requestLink = "https://localhost:44328/api/tripsapi/";
+
+
+        public IEnumerable<Trip> GetAllTrips()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Trip> GetApprovedTrips()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Trip> GetDeniedTrips()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Trip> GetPendingTrips()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Trip GetTripById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTrip(int id, Trip trip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTrip(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTrip(Trip trip)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessTripAdministration/Models/IApiClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessTripApplication.Migrations;
+using BusinessTripApplication.Models;
+
+namespace BusinessTripAdministration.Models
+{
+    public interface IApiClient
+    {
+
+        IEnumerable<Trip> GetAllTrips();
+        IEnumerable<Trip> GetApprovedTrips();
+        IEnumerable<Trip> GetDeniedTrips();
+        IEnumerable<Trip> GetPendingTrips();
+        Trip GetTripById(int id);
+        void UpdateTrip(int id, Trip trip);
+        void DeleteTrip(int id);
+        void AddTrip(Trip trip);
+
+
+
+    }
+}
