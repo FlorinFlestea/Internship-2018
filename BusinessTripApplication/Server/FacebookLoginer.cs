575260a3951282e9141822f233c8e486ebe2b237
csaba97 <37183688+csaba97@users.noreply.github.com>
2018-08-08 12:29:36 +0300

    changes

--- BusinessTripApplication/Server/FacebookLoginer.cs
+++ BusinessTripApplication/Server/FacebookLoginer.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Helpers;
 using System.Web.Security;
-using BusinessTripApplication.Exception;
 using BusinessTripApplication.Repository;
-using BusinessTripModels;
+using BusinessTripModels.Models;
 using Facebook;
 
 namespace BusinessTripApplication.Server
